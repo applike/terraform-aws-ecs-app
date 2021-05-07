602cc6caa69076134964b72888a916c7dfe97778
Sebastian Elm <sebastian@justdice.io>
2021-05-07 10:35:29 +0200

    fix: add average and max resource alarms;

---
+++ alarms.tf
@@ -0,0 +1,17 @@
+module "alarm-service-resources" {
+  source                               = "applike/alarm-service-resources/aws"
+  version                              = "1.0.0"
+  context                              = module.this.context
+  enabled                              = var.resource_alarms_enabled
+  treat_missing_data                   = var.resource_alarms_treat_missing_data
+  average_resource_period              = var.resource_alarms_average_period
+  average_resource_evaluation_periods  = var.resource_alarms_average_evaluation_periods
+  average_resource_datapoints_to_alarm = var.resource_alarms_average_datapoints_to_alarm
+  average_resource_cpu_threshold       = var.resource_alarms_average_cpu_threshold
+  average_resource_memory_threshold    = var.resource_alarms_average_memory_threshold
+  maximum_resource_period              = var.resource_alarms_maximum_period
+  maximum_resource_evaluation_periods  = var.resource_alarms_maximum_evaluation_periods
+  maximum_resource_datapoints_to_alarm = var.resource_alarms_maximum_datapoints_to_alarm
+  maximum_resource_cpu_threshold       = var.resource_alarms_maximum_cpu_threshold
+  maximum_resource_memory_threshold    = var.resource_alarms_maximum_memory_threshold
+}
